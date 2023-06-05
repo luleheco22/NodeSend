5b99d325ad6e9861311932bd246fec1f19fb179f
leonel <leonelhcontreras@gmail.com>
2023-06-05 14:53:43 -0400

    ajuste en la url del backend

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    backendURL: 'https://nodesendbackend-production-7232.up.railway.app',
+    backendURL: 'http://18.204.204.133:3001',
     frontendURL: 'https://node-send-lilac.vercel.app'
   }
 }
